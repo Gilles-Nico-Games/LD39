09210025a893fbb8e7ec7da896a18d95253a09d9
NICO\vnico <nicolas.vienat@ynov.com>
2017-07-29 15:27:30 +0200

    Gestion de la camera + mort du player si il tombe

---
+++ Assets/Scripts/Player/Energy_Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Energy_Player : MonoBehaviour {
+
+    public int energy;
+    public bool hasDied;
+	// Use this for initialization
+	void Start () {
+        hasDied = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameObject.transform.position.y < -7)
+            hasDied = true;
+
+        if (hasDied == true)
+            StartCoroutine("Die");
+	}
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("Main");
+        yield return null;
+        
+    }
+}
