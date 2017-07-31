9cd1d9c27ed06001d205b7a3f17f307edffb757a
VIENAT Nicolas <nicolas.vienat@ynov.com>
2017-07-31 17:39:37 +0200

    Commit background

--- Assets/Scripts/Ennemy/Move_Ennemy.cs
+++ Assets/Scripts/Ennemy/Move_Ennemy.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class Move_Ennemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int ennemySpeed = 10;
+    public int ennemyJumpPower = 1250;
+
+    public bool facingRight = true;
+    public bool isGrounded;
+
+    // Update is called once per frame
+    void Update () {
+
+        //Gestion des la physiques
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(ennemySpeed, gameObject.GetComponent<Rigidbody>().velocity.y, 0);
+    }
+
+    void FlipEnnemy()
+    {
+        facingRight = !facingRight;
+        Vector3 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        Debug.Log("Player has collided with : " + collider.collider.tag);
+        if (collider.gameObject.tag == "colliderGround")
+        {
+            isGrounded = true;
+        }
+    }
 }
